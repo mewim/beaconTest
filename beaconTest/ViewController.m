5a6a7f9777898cbe21d9f43178510d55198f9505
mewim <cliu81@illinois.edu>
2016-06-08 16:23:40 -0500

    Edge cases

--- beaconTest/ViewController.m
+++ beaconTest/ViewController.m
@@ -135,7 +135,40 @@
     }
     
     NSString *disp_text, *refined_text;
-    if([sortedBeacons count] >= 3){
+    userCoord currCoord;
+    currCoord.x = -1.0f;
+    currCoord.y = -1.0f;
+
+    
+    if([sortedBeacons count] == 1){ // Only one beacon, just use its location
+        // Just use the beacon as user location
+        NSMutableArray *userCoordArr = [self getPoint:currCoord.x joinY:currCoord.y];
+        currCoord.x = [userCoordArr[0] floatValue];
+        currCoord.y = [userCoordArr[1] floatValue];
+    }
+    else if([sortedBeacons count] == 2){ // Two beacons
+        ESTBeacon* firstBeacon = sortedBeacons[0], *secondBeacon = sortedBeacons[1];
+        coord firstCoord = [_beconDict getCoord:[[firstBeacon major] integerValue] joinMinor:[[firstBeacon minor] integerValue]];
+        coord secondCoord = [_beconDict getCoord:[[secondBeacon major] integerValue] joinMinor:[[secondBeacon minor] integerValue]];
+        NSMutableArray *firstCoordArr = [self getPoint:firstCoord.x joinY:firstCoord.y];
+        NSMutableArray *secondCoordArr = [self getPoint:secondCoord.x joinY:secondCoord.y];
+        
+        // Reference: http://math.stackexchange.com/questions/256100/how-can-i-find-the-points-at-which-two-circles-intersect
+        double x1 = [firstCoordArr[0] floatValue];
+        double y1 = [firstCoordArr[1] floatValue];
+        double x2 = [secondCoordArr[0] floatValue];
+        double y2 = [secondCoordArr[1] floatValue];
+        double r1 = [firstBeacon accuracy];
+        double r2 = [secondBeacon accuracy];
+        double d = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+        double l = (pow(r1, 2) - pow(r2, 2) + pow(d, 2)) / (2 * d);
+//        double h = sqrt(pow(r1, 2) + pow(l, 2));
+
+        currCoord.x = (l / d) * (x2 - x1) + x1;
+        currCoord.y = (l / d) * (y2 - y1) + y1;
+
+    }
+    else if([sortedBeacons count] >= 3){ // Three beacons or more
         ESTBeacon* firstBeacon = sortedBeacons[0], *secondBeacon = sortedBeacons[1], *thirdBeacon = sortedBeacons[2];
         coord firstCoord = [_beconDict getCoord:[[firstBeacon major] integerValue] joinMinor:[[firstBeacon minor] integerValue]];
         coord secondCoord = [_beconDict getCoord:[[secondBeacon major] integerValue] joinMinor:[[secondBeacon minor] integerValue]];
@@ -144,34 +177,54 @@
             thirdBeacon = sortedBeacons[i];
             thirdCoord = [_beconDict getCoord:[[thirdBeacon major] integerValue] joinMinor:[[thirdBeacon minor] integerValue]];
         }
-        NSMutableArray *P1 = [self getPoint:firstCoord.x joinY:firstCoord.y];
-        NSMutableArray *P2 = [self getPoint:secondCoord.x joinY:secondCoord.y];
-        NSMutableArray *P3 = [self getPoint:thirdCoord.x joinY:thirdCoord.y];
-        float DistA = [firstBeacon accuracy];
-        float DistB = [secondBeacon accuracy];
-        float DistC = [thirdBeacon accuracy];
-        userCoord coord = trilateration(P1, P2, P3, DistA, DistB, DistC);
-        
-        if (!isnan(coord.x) && !isnan(coord.y)){
-            if(_refiner == nil){
-                _refiner = [[PositionRefiner alloc] init];
-            }
-            userCoord refinedCoord = [_refiner refinePosition:coord];
-            disp_text = [NSString stringWithFormat:@"User's Location:\nx: %f, y: %f", coord.x, coord.y];
-            refined_text= [NSString stringWithFormat:@"Refined Location:\nx: %f, y: %f", refinedCoord.x, refinedCoord.y];
+        if(((firstCoord.x == secondCoord.x) && (secondCoord.x == thirdCoord.x)) || ((firstCoord.y == secondCoord.y) && (secondCoord.y == thirdCoord.y))){ // We only have three beacons in a line, use two closest
+            NSMutableArray *firstCoordArr = [self getPoint:firstCoord.x joinY:firstCoord.y];
+            NSMutableArray *secondCoordArr = [self getPoint:secondCoord.x joinY:secondCoord.y];
             
-            userCoord ORIGIN = {0.0, 0.0};
-            userCoord canvasDims = {320, 320};
-            userCoord MAP_DIMS = {56.244f, 55.9f};
-            userCoord translated = {-1.0f, -1.0f};
+            // Reference: http://math.stackexchange.com/questions/256100/how-can-i-find-the-points-at-which-two-circles-intersect
+            double x1 = [firstCoordArr[0] floatValue];
+            double y1 = [firstCoordArr[1] floatValue];
+            double x2 = [secondCoordArr[0] floatValue];
+            double y2 = [secondCoordArr[1] floatValue];
+            double r1 = [firstBeacon accuracy];
+            double r2 = [secondBeacon accuracy];
+            double d = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+            double l = (pow(r1, 2) - pow(r2, 2) + pow(d, 2)) / (2 * d);
+            //        double h = sqrt(pow(r1, 2) + pow(l, 2));
             
-            translated.x = (float) (refinedCoord.x + ORIGIN.x) * canvasDims.x / MAP_DIMS.x - 10.0;
-            translated.x = (translated.x < 0)? 0: translated.x;
-            translated.y = (float) (refinedCoord.y + ORIGIN.y) * canvasDims.y / MAP_DIMS.y + 154.0;
-            CGRect newLocation = CGRectMake(translated.x, translated.y, 20, 20);
-            userLocation.frame = newLocation;
-            [self.view addSubview:userLocation];
+            currCoord.x = (l / d) * (x2 - x1) + x1;
+            currCoord.y = (l / d) * (y2 - y1) + y1;
         }
+        else{ // We can do Trilateration
+            NSMutableArray *P1 = [self getPoint:firstCoord.x joinY:firstCoord.y];
+            NSMutableArray *P2 = [self getPoint:secondCoord.x joinY:secondCoord.y];
+            NSMutableArray *P3 = [self getPoint:thirdCoord.x joinY:thirdCoord.y];
+            float DistA = [firstBeacon accuracy];
+            float DistB = [secondBeacon accuracy];
+            float DistC = [thirdBeacon accuracy];
+            currCoord = trilateration(P1, P2, P3, DistA, DistB, DistC);
+        }
+
+    }
+    if (!isnan(currCoord.x) && !isnan(currCoord.y) && currCoord.x > 0 && currCoord.y > 0 && [sortedBeacons count] > 0){
+        if(_refiner == nil){
+            _refiner = [[PositionRefiner alloc] init];
+        }
+        userCoord refinedCoord = [_refiner refinePosition:currCoord];
+        disp_text = [NSString stringWithFormat:@"User's Location:\nx: %2f, y: %2f", currCoord.x, currCoord.y];
+        refined_text= [NSString stringWithFormat:@"Refined Location:\nx: %2f, y: %2f", refinedCoord.x, refinedCoord.y];
+        
+        userCoord ORIGIN = {0.0, 0.0};
+        userCoord canvasDims = {320, 320};
+        userCoord MAP_DIMS = {56.244f, 55.9f};
+        userCoord translated = {-1.0f, -1.0f};
+        
+        translated.x = (float) (refinedCoord.x + ORIGIN.x) * canvasDims.x / MAP_DIMS.x - 10.0;
+        translated.x = (translated.x < 0)? 0: translated.x;
+        translated.y = (float) (refinedCoord.y + ORIGIN.y) * canvasDims.y / MAP_DIMS.y + 154.0;
+        CGRect newLocation = CGRectMake(translated.x, translated.y, 20, 20);
+        userLocation.frame = newLocation;
+        [self.view addSubview:userLocation];
     }
     else{
         disp_text= [NSString stringWithFormat:@"User's Location:\n unavailable"];
